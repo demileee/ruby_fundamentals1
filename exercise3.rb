eea1bb392f036bba2f20bca363d166e8ea482d15
Demi Lee <demilee.to@gmail.com>
2017-09-18 11:59:00 -0400

    exercise 3, +gets yearofbirth, calculates and puts age, also add comments

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,9 @@
+#Ask and print name
 puts "What is your name?"
 user_name = gets
 puts "Hello, #{user_name}"
+
+#Ask for year of birth, print age
+puts "What year were you born?"
+yearOfBirth = gets
+puts "You are #{2017 - yearOfBirth.to_i} years old."
