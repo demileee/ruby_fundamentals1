46344229caddd680ab01136e754bd97f524f03d3
Demi Lee <demilee.to@gmail.com>
2017-09-18 11:48:18 -0400

    exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,14 @@
+#1 How would you calculate a good tip for a 55 dollar meal? Use puts to print the answer.
+puts "A good tip of 20% for a 55 dollar meal would equate to $#{55*0.2}."
+
+#2 Try adding a string and add an integer with the + operator. What happens? Find a way to
+#convert the integer into a string first and use puts to print the result.
+puts "2".to_i + 2
+
+#3 Try outputting the result of 45628 multiplied by 7839 in a sentence by using string
+#interpolation.
+puts "#{45628 * 7839}"
+
+#4 What's the value of the expression (10 < 10 && 30 < 30) || !(10 == 11)? Try figuring it
+#out on your own before typing it in.
+puts (10 < 10 && 30 < 30) || !(10 == 11)
