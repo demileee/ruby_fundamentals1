5701ee34133d91a4b196f3ee82e8fe021b54e8b0
Demi Lee <demilee.to@gmail.com>
2017-09-18 13:04:31 -0400

    exercise 6.2

--- exercise6.rb
+++ exercise6.rb
@@ -1,20 +1,29 @@
 distance = 6
+energy = 100
 
-while true
+while energy > 0
 
   puts "Will you walk or run?"
   activity = gets.chomp.to_s
 
   if activity == "walk"
     distance += 1
+    energy += 10
     puts "Distance from home is #{distance}km."
+    puts "Energy is at #{energy}%"
   elsif activity == "run"
     distance += 5
+    energy -= 20
     puts "Distance from home is #{distance}km."
+    puts "Energy is at #{energy}%"
   else
     break if activity == "go home"
     puts "I didn't get that."
   end
 end
 
-puts "You have gone home."
+if energy <= 0
+  puts "You have run out of energy."
+else
+  puts "You have gone home."
+end
